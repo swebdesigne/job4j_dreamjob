353ac1d02cdfeef5d860bd1ed6ff8e3dc4454eed
swebdesigne <swebdesigne@gmail.com>
2022-10-26 19:01:44 +0300

    4. Thymeleaf, Циклы.

---
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -0,0 +1,17 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.store.PostStore;
+
+@Controller
+public class PostController {
+    private final PostStore postStore = PostStore.instOf();
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", postStore.findAll());
+        return "posts";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,70 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate created;
+
+    public Post() {
+    }
+
+    public Post(int id, String name, String description, LocalDate created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
